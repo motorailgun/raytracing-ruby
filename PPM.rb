114f87fe3b9e08a2bd40f089d3b68ab68cfe9d79
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-02-23 08:41:27 +0900

    PPM.rb: very early PPM implemention

---
+++ PPM.rb
@@ -0,0 +1,19 @@
+class PPM
+    def initialize(width: 640, height: 480, level: 256, default: 0)
+        @canvas = Array.new(height){ Array.new(width){ [default, default, default] } }
+        @width, @height = width, height
+        @level = level
+    end
+
+    def to_s
+        "P3\n#{@width} #{@height}\n#{@level}".concat(
+            @canvas.reduce(""){|str, array|
+                str + array.reduce("\n"){|str, rgb| str + " " + rgb.join(" ") }
+            }
+        )
+    end
+
+    def [](idx)
+        @canvas[idx]
+    end
+end
