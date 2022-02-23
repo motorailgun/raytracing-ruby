0d0799b9f076f58269b3f428e505aa5fca776d11
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-02-23 14:38:47 +0900

    PPM.rb: using RGB class

--- PPM.rb
+++ PPM.rb
@@ -1,6 +1,8 @@
+require './RGB.rb'
+
 class PPM
     def initialize(width: 640, height: 480, level: 256, default: 0)
-        @canvas = Array.new(height){ Array.new(width){ [default, default, default] } }
+        @canvas = Array.new(height){ Array.new(width){ RGB.new(default) } }
         @width, @height = width, height
         @level = level
     end
