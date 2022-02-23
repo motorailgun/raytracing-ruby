f73325d3fdb4b4ae04e30aeffb3ad1b439d63ec0
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-02-23 14:37:41 +0900

    RGB.rb: little fix with alias_method

--- RGB.rb
+++ RGB.rb
@@ -3,9 +3,7 @@ require './Triplet.rb'
 class RGB < Triplet
     def initialize(init_value: 0)
         [[:r, "@x"], [:g, "@y"], [:b, "@z"]].each{|ar|
-            define_singleton_method(ar[0]){
-                eval("#{ar[1]}")
-            }
+            alias_method ar[0], ar[1]
         }
 
         super
