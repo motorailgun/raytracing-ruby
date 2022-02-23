3041552acb56ddf977b38756e453d9690fcc56f3
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-02-23 17:39:01 +0900

    Triplet.rb: add methods for calcuration

--- Triplet.rb
+++ Triplet.rb
@@ -1,8 +1,62 @@
 class Triplet
     attr_accessor :x, :y, :z
 
-    def initialize(init_value = 0)
-        @x, @y, @z = init_value, init_value, init_value
+    # methods required for calcuration
+    def -@
+        self.class.new(-@x, -@y, -@z)
+    end
+
+    def **(num)
+        @x ** num + @y ** num + @z ** num
+    end
+    
+    def plus_equal(ins)
+        @x += ins.x
+        @y += ins.y
+        @z += ins.z
+    end
+
+    def minus_equal(ins)
+        @x -= ins.x
+        @y -= ins.y
+        @z -= ins.z
+    end
+
+    def mtply_equal(num)
+        @x *= num
+        @y *= num
+        @z *= num
+    end
+
+    def div_equal(num)
+        @x /= num
+        @y /= num
+        @z /= num
+    end
+
+    [:+, :-].each{|sym|
+        define_method(sym){|operand|
+            self.class.new(
+                @x.send(sym, operand.x),
+                @y.send(sym, operand.y),
+                @z.send(sym, operand.z) 
+            )
+        }
+    }
+
+    ['*', '/'].each{|sym|
+        define_method(sym){|operand|
+            self.class.new(
+                @x.send(sym, operand),
+                @y.send(sym, operand),
+                @z.send(sym, operand) 
+            )
+        }
+    }
+
+    # normal (and utilities) methods below
+    def initialize(x = 0, y = nil, z = nil)
+        @x, @y, @z = x, (y || x), (z || x)
     end
 
     def [](idx)
