e40fb78c4c3fb62929e707685a5588422d74e055
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-02-23 16:20:39 +0900

    RGB.rb: fix alias calling

--- RGB.rb
+++ RGB.rb
@@ -1,12 +1,12 @@
 require './Triplet.rb'
 
 class RGB < Triplet
+    [[:r, :x], [:g, :y], [:b, :z]].each{|ar|
+        eval("alias #{ar[0]} #{ar[1]}")
+        eval("alias #{ar[0]}= #{ar[1]}=")
+    }
+
     def initialize(init_value = 0)
         super
-
-        [[:r, :x], [:g, :y], [:b, :z]].each{|ar|
-            eval("alias #{ar[0]} #{ar[1]}")
-            eval("alias #{ar[0]}= #{ar[1]}=")
-        }
     end
 end
