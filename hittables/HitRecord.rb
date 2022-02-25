59ffa3add1b4f90153856cb7190f80cffd7ae494
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-02-25 21:29:47 +0900

    HitRecord: fix bad variable name pt.2

--- hittables/HitRecord.rb
+++ hittables/HitRecord.rb
@@ -1,7 +1,7 @@
 class HitRecord
-    attr_accessor :hit_point, :normal, :t, :front_face?
+    attr_accessor :hit_point, :normal, :t, :front_face
 
     def initialize(hit_point = nil, normal = nil, t = nil, front_face = nil)
-        @hit_point, @normal, @t, @front_face? = hit_point, normal, t, front_face
+        @hit_point, @normal, @t, @front_face = hit_point, normal, t, front_face
     end
 end
