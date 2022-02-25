c4b161e74258168a7b3f3d322a3e65e88026849a
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-02-25 19:42:22 +0900

    Vector3D: add ==

--- Vector3D.rb
+++ Vector3D.rb
@@ -4,6 +4,14 @@ class Vector3D < Triplet
     def initialize(x = 0.0, y = nil, z = nil)
         super
     end
+
+    def ==(other)
+        if other.kind_of?(self.class)
+            @x == other.x && @y == other.y && @z == other.z
+        else
+            false
+        end
+    end
 end
 
 def v3d(x = 0.0, y = nil, z = nil)
