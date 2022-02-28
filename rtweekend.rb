e9a8e936ea1637fa624e437b0bb5aa14faebd550
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-02-28 15:53:42 +0900

    run/rtweekend: move top-level methods to rtweekend

--- rtweekend.rb
+++ rtweekend.rb
@@ -16,3 +16,18 @@ def randvec_in_unit_sphere
         return v
     }
 end
+
+def ray_color(ray, world)
+    if hit_record = world.hit(ray, 0.0, Infinity) then
+        return  0.5 * (rgb(hit_record.normal) + rgb(1.0, 1.0, 1.0));
+    end
+    unit_direction = ray.direction.unit_vector
+    seppen = 0.5 * (unit_direction.y + 1.0)
+    (1.0 - seppen) * rgb(1.0, 1.0, 1.0) + seppen * rgb(0.5, 0.7, 1.0)
+end
+
+def clamp_color(color, samples)
+    color * (1.0 / samples)
+    # ^^ must be faster than deviding three(r, g, b) ?
+    # this calculation is done for every pixel, so faster must be better
+end
--- run.rb
+++ run.rb
@@ -1,20 +1,5 @@
 require './rtweekend'
 
-def ray_color(ray, world)
-    if hit_record = world.hit(ray, 0.0, Infinity) then
-        return  0.5 * (rgb(hit_record.normal) + rgb(1.0, 1.0, 1.0));
-    end
-    unit_direction = ray.direction.unit_vector
-    seppen = 0.5 * (unit_direction.y + 1.0)
-    (1.0 - seppen) * rgb(1.0, 1.0, 1.0) + seppen * rgb(0.5, 0.7, 1.0)
-end
-
-def clamp_color(color, samples)
-    color * (1.0 / samples)
-    # ^^ must be faster than deviding three(r, g, b) ?
-    # this calculation is done for every pixel, so faster must be better
-end
-
 # image properties
 aspect_ratio = 16.0 / 9.0
 image_width = 400
