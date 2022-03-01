e7c4c74a69c4b875cd1b2b291d1adccd8ba7de4d
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-01 18:40:54 +0900

    HitRecord: add material

--- hittables/HitRecord.rb
+++ hittables/HitRecord.rb
@@ -1,7 +1,7 @@
 class HitRecord
     attr_accessor :hit_point, :normal, :t, :front_face
 
-    def initialize(hit_point = nil, normal = nil, t = nil, front_face = nil)
-        @hit_point, @normal, @t, @front_face = hit_point, normal, t, front_face
+    def initialize(hit_point = nil, normal = nil, t = nil, front_face = nil, material = nil)
+        @hit_point, @normal, @t, @front_face, @material = hit_point, normal, t, front_face, material
     end
 end
