874d3923798756973688e86140b7b88eb60474d0
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-01 18:49:39 +0900

    HitRecord: fix attr_accessor

--- hittables/HitRecord.rb
+++ hittables/HitRecord.rb
@@ -1,5 +1,5 @@
 class HitRecord
-    attr_accessor :hit_point, :normal, :t, :front_face
+    attr_accessor :hit_point, :normal, :t, :front_face, :material
 
     def initialize(hit_point = nil, normal = nil, t = nil, front_face = nil, material = nil)
         @hit_point, @normal, @t, @front_face, @material = hit_point, normal, t, front_face, material
