986782313dc443bb3ea2496c8007798c0935b921
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-01 19:19:46 +0900

    Lambertian: fix some

--- materials/Lambertian.rb
+++ materials/Lambertian.rb
@@ -6,7 +6,7 @@ class Lambertian < Material
     end
 
     def reflect(ray, hit_record)
-        scatter_direction = hit_record.normal + random_unit_vector
+        scatter_direction = hit_record.normal + rand_unit_vector
         scatter_direction = hit_record.normal if scatter_direction.near_zero?
         ray(hit_record.hit_point, scatter_direction)
     end
