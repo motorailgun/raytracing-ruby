f9dde1a182d547d0200847a60f8a8034c7193b8b
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-01 19:12:00 +0900

    Metal: fix variable name

--- materials/Metal.rb
+++ materials/Metal.rb
@@ -7,7 +7,7 @@ class Metal < Material
 
     def reflect(ray, hit_record)
         reflect_direction = metal_reflect(ray.direction.unit_vector, hit_record.normal)
-        reflected_ray = ray(hit_record.hit_point, scatter_direction)
+        reflected_ray = ray(hit_record.hit_point, reflect_direction)
         return reflected_ray.direction.dot(hit_record.normal) > 0 && reflected_ray
     end
 
