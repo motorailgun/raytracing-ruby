1f06921a7ee01b80ac6b9b89552f89f2da5c9c8b
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-02 01:19:48 +0900

    Metal: implement roughness

--- materials/Metal.rb
+++ materials/Metal.rb
@@ -1,13 +1,14 @@
 require_relative '../rtweekend'
 
 class Metal < Material
-    def initialize(attenuation = rgb(0.99))
+    def initialize(attenuation = rgb(0.99), roughness = 0.0)
         @attenuation = attenuation
+        @roughness = roughness > 1.0 ? 1.0 : roughness
     end
 
     def reflect(ray, hit_record)
         reflect_direction = metal_reflect(ray.direction.unit_vector, hit_record.normal)
-        reflected_ray = ray(hit_record.hit_point, reflect_direction)
+        reflected_ray = ray(hit_record.hit_point, reflect_direction + randvec_in_unit_sphere * @roughness)
         return reflected_ray.direction.dot(hit_record.normal) > 0 && reflected_ray
     end
 
