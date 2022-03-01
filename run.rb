3f3e76db3c4e10802fb5bd034ee288c6ff3c409e
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-01 19:03:49 +0900

    run: move common part to preperation

--- run.rb
+++ run.rb
@@ -1,20 +1,5 @@
-require './rtweekend'
+require './preperation'
 
-# image properties
-aspect_ratio = 16.0 / 9.0
-image_width = 400
-image_height = Integer(image_width / aspect_ratio)
-
-# world
-world = HittableList.new
-world.add(Sphere.new(p3d(0.0, 0.0, -1.0), 0.5))
-world.add(Sphere.new(p3d(0.0, -100.5, -1.0), 100.0))
-
-# camera properties
-camera = Camera.new(:height => 2.0, :focal_length => 1.0)
-samples = 100
-
-max_depth = 25
 canvas = PPM.new(image_width, image_height)
 
 image_height.times{|h|
--- run_mp.rb
+++ run_mp.rb
@@ -1,21 +1,4 @@
-require './rtweekend'
-require 'pp'
-
-# image properties
-aspect_ratio = 16.0 / 9.0
-image_width = 400
-image_height = Integer(image_width / aspect_ratio)
-
-# world
-world = HittableList.new
-world.add(Sphere.new(p3d(0.0, 0.0, -1.0), 0.5))
-world.add(Sphere.new(p3d(0.0, -100.5, -1.0), 100.0))
-
-# camera properties
-camera = Camera.new(:height => 2.0, :focal_length => 1.0)
-samples = 50
-
-max_depth = 25
+require './preperation'
 
 pids = []
 readers = []
