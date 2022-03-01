ef04c3f7ae9381499677bb36ea2f1fa5cf5533cf
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-01 18:53:37 +0900

    Lambertian: fix method call for reflect()

--- materials/Lambertian.rb
+++ materials/Lambertian.rb
@@ -5,7 +5,7 @@ class Lambertian < Material
         @attenuation = attenuation
     end
 
-    def reflect(hit_record)
+    def reflect(ray, hit_record)
         scatter_direction = hit_record.normal + random_unit_vector
         scatter_direction = hit_record.normal if scatter_direction.near_zero?
         ray(hit_record.hit_point, scatter_direction)
