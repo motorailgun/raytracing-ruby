5bb9f7349e5bf4ccb064443e8313ef6a9ab548d7
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-02 01:37:46 +0900

    run: use Metal roughness

--- run_mp.rb
+++ run_mp.rb
@@ -10,8 +10,8 @@ world = HittableList.new
 
 material_ground = Lambertian.new(rgb(0.8, 0.8, 0.0))
 material_center = Lambertian.new(rgb(0.7, 0.3, 0.3))
-material_left = Metal.new(rgb(0.8, 0.8, 0.8))
-material_right = Metal.new(rgb(0.8, 0.6, 0.2))
+material_left = Metal.new(rgb(0.8, 0.8, 0.8), 0.3)
+material_right = Metal.new(rgb(0.8, 0.6, 0.2), 1.0)
 
 world.add(Sphere.new(p3d(0.0, -100.5, -1.0), 100.0, material_ground))
 world.add(Sphere.new(p3d(0.0, 0.0, -1.0), 0.5, material_center))
