bdada14fb96b754a44d52510f953c2e4d0fbb872
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-11 17:54:15 +0900

    run/run_mp: change some

--- run.rb
+++ run.rb
@@ -9,9 +9,9 @@ image_height = Integer(image_width / aspect_ratio)
 world = HittableList.new
 
 material_ground = Lambertian.new(rgb(0.8, 0.8, 0.0))
-material_center = Dielectric.new(1.5)
+material_center = Lambertian.new(rgb(0.1, 0.2, 0.5))
 material_left = Dielectric.new(1.5)
-material_right = Metal.new(rgb(0.8, 0.6, 0.2), 1.0)
+material_right = Metal.new(rgb(0.8, 0.6, 0.2), 0.0)
 
 world.add(Sphere.new(p3d(0.0, -100.5, -1.0), 100.0, material_ground))
 world.add(Sphere.new(p3d(0.0, 0.0, -1.0), 0.5, material_center))
