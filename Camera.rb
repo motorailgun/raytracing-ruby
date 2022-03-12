fcf17a073936c9d28dc3d3276ea77c76d334ac97
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-12 14:24:07 +0900

    Camera: make camera movable

--- Camera.rb
+++ Camera.rb
@@ -6,7 +6,10 @@ class Camera
     def initialize(aspect_ratio: 16.0 / 9.0,
                   focal_length: 1.0,
                   origin: p3d(0.0, 0.0, 0.0),
-                  v_fov: 90
+                  v_fov: 90,
+                  look_from: ,
+                  look_at: ,
+                  vec_view_up: 
                 )
         
         raise ArgumentError.new("Origin must be kind of Point3D") unless origin.kind_of?(Point3D)
@@ -18,9 +21,14 @@ class Camera
         @aspect_ratio, @focal_length = aspect_ratio, focal_length
         @origin = origin
 
-        @horizontal = p3d(@viewport_width, 0.0, 0.0)
-        @vertical = p3d(0.0, @viewport_height, 0.0)
-        @lower_left_corner = @origin - @horizontal / 2.0 - @vertical / 2.0 - p3d(0.0, 0.0, @focal_length)
+        w = (look_from - look_at).unit_vector
+        u = vec_view_up.cross(w).unit_vector
+        v = w.cross(u)
+
+        @origin = look_from
+        @horizontal = @viewport_width * u
+        @vertical = @viewport_height * v
+        @lower_left_corner = @origin - @horizontal / 2 - @vertical / 2 - w
     end
 
     def get_ray(u, v)
