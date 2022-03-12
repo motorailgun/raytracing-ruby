06a3298f66df12a079d9f59e88bce624e159c2b8
motorailgun <28751910+motorailgun@users.noreply.github.com>
2022-03-12 14:24:26 +0900

    run/run_mp: adopt Camera changes

--- run.rb
+++ run.rb
@@ -7,16 +7,24 @@ image_height = Integer(image_width / aspect_ratio)
 
 # world
 world = HittableList.new
-radius = Math.cos(Math::PI / 4.0)
 
-material_left = Lambertian.new(rgb(0.0, 0.0, 1.0))
-material_right = Lambertian.new(rgb(1.0, 0.0, 0.0))
+material_ground = Lambertian.new(rgb(0.8, 0.8, 0.0))
+material_center = Lambertian.new(rgb(0.1, 0.2, 0.5))
+material_left = Dielectric.new(1.5)
+material_right = Metal.new(rgb(0.8, 0.6, 0.2), 0.0)
 
-world.add(Sphere.new(p3d(-radius, 0.0, -1.0), radius, material_left))
-world.add(Sphere.new(p3d(radius, 0.0, -1.0), radius, material_right))
+world.add(Sphere.new(p3d(0.0, -100.5, -1.0), 100.0, material_ground))
+world.add(Sphere.new(p3d(0.0,    0.0, -1.0),   0.5, material_center))
+world.add(Sphere.new(p3d(-1.0,   0.0, -1.0),   0.5, material_left))
+world.add(Sphere.new(p3d(-1.0,   0.0, -1.0), -0.45, material_left))
+world.add(Sphere.new(p3d(1.0,    0.0, -1.0),   0.5, material_right))
 
 # camera properties
-camera = Camera.new(:v_fov => 90, :aspect_ratio => aspect_ratio)
+camera = Camera.new(:v_fov => 90, :aspect_ratio => aspect_ratio,
+                    :look_from => p3d(-2.0, 2.0, 1.0),
+                    :look_at => p3d(0.0, 0.0, -1.0),
+                    :vec_view_up => v3d(0.0, 1.0, 0.0)
+                )
 samples = 50
 
 max_depth = 25
